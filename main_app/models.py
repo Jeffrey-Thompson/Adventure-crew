42c8d5a09abc3ef73f28adadc9a1bd786da5c63e
JeffreyThompson <jeffathomp@gmail.com>
2020-10-06 15:36:35 -0500

    enemy model

--- main_app/models.py
+++ main_app/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class Adventurer(models.Model):
+class Enemy(models.Model):
     name = models.CharField(max_length=100)
+    species = model.CharField(max_length=100)
+    treasure = model.IntegerField()
+    health = model.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Adventurer(models.Model):
+    name = models.CharField(max_length=100, unique=True)
     style = models.CharField('combat style', max_length=100)
     weapon = models.CharField('perferred weapon', max_length=100)
     health = models.IntegerField()
